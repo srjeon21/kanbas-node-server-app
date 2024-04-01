83e6ba33b52fad8c7af5baf965f5f92a502f60a4
Serin Serena Jeon <serin0314@gmail.com>
2024-04-01 16:49:36 -0400

    trying to make new repo

---
+++ App.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import Hello from './Hello.js';
+import Lab5 from './Lab5.js';
+import cors from "cors";
+import CourseRoutes from './Kanbas/Courses/routes.js';
+import ModuleRoutes from './Kanbas/Modules/routes.js';
+
+const app = express();
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+Hello(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
