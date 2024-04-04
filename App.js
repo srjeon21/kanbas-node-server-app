6fd793e60d4226e7ce6c2d7964eaab86c0d413f4
Serin Serena Jeon <serin0314@gmail.com>
2024-04-03 23:11:29 -0400

    doing 3.5.1

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import UserRoutes from './Kanbas/Users/routes.js';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import cors from "cors";
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(
     cors({
@@ -13,6 +16,7 @@ app.use(
     })
   );
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 Hello(app);
